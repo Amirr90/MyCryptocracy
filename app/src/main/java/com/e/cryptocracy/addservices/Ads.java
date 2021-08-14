a970e4b3f11501fb8ed7c75243ee75f304403ad2
Aamirr <aamirr.1232@gmail.com>
2021-08-14 15:22:18 +0530

    GettingStartedPage

--- app/src/main/java/com/e/cryptocracy/addservices/Ads.java
+++ app/src/main/java/com/e/cryptocracy/addservices/Ads.java
@@ -2,7 +2,7 @@ package com.e.cryptocracy.addservices;
 
 public class Ads {
     //test
-    //public static final String BANNER_ADD_ID = "ca-app-pub-3940256099942544/6300978111";
+    //=\// lic static final String BANNER_ADD_ID = "ca-app-pub-3940256099942544/6300978111";
 
     //live
      public static final String BANNER_ADD_ID = "ca-app-pub-5778282166425967/2981756838";
